453483a5892523a1ec90b0d3df93b5659efe97cb
alexxx <puyaalex@gmail.com>
2024-07-15 15:35:24 +0300

    first commit

---
+++ Helpers/DiServices.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFirstApi.Helpers;
+
+using MyFirstApi.Services;
+
+public static class DiServices
+{
+    public static IServiceCollection AddDiServices(this IServiceCollection services)
+    {
+        services.AddScoped<IPostService, PostsService>();
+
+        return services;
+    }
+}
